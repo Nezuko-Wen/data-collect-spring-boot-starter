534bd729351a43779366622dcdc685d00e23eea4
Wen <409053122@qq.com>
2022-10-12 18:38:13 +0800

    init

--- src/main/java/com/zzw/datacollect/DataCollectConfiguration.java
+++ src/main/java/com/zzw/datacollect/DataCollectConfiguration.java
@@ -1,14 +1,17 @@
 package com.zzw.datacollect;
 
 import com.zzw.datacollect.test.DemoService;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author Wen
  * @date 2022/10/12 15:17
  */
 @Configuration
+@MapperScan("com.zzw.datacollect.mapper")
 public class DataCollectConfiguration {
     @Bean
     public DemoService demoService() {
